ac70c247800fb7d0eb380cf616dc423a2d02c3a5
Yashsoni <sonyyash19@gmail.com>
2021-09-01 23:05:29 +0530

    Removed unneccesary log

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javascript has loaded in!");
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
